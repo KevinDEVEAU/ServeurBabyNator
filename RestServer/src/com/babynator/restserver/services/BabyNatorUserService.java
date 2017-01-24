3dda488fe611b67fe6f9f0ed06278e849cb5fd8f
Kevin <Kevin @172.16.14.83>
2017-01-24 10:47:00 +0100

    Ajout du projet

---
+++ RestServer/src/com/babynator/restserver/services/BabyNatorUserService.java
@@ -0,0 +1,30 @@
+package com.babynator.restserver.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.babynator.restserver.BabyNatorUser;
+
+@Path("/users")
+public class BabyNatorUserService {
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public BabyNatorUser getUser( @PathParam("id") int id ) {
+    	BabyNatorUser user = new BabyNatorUser(id, "jdoe", 22);
+        return user;
+    }
+    
+    @POST
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public BabyNatorUser postUser( BabyNatorUser user ) {
+    	System.out.println(user);
+        return user; 
+    }
+}
+
