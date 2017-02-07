2925368514db0340018046aa6e5ec719cac2ad13
Kevin <deveaukevin77@gmail.com>
2017-02-07 16:56:34 +0100

    modif data

--- RestServer/src/com/babynator/restserver/db/DataDAO.java
+++ RestServer/src/com/babynator/restserver/db/DataDAO.java
@@ -2,11 +2,15 @@ package com.babynator.restserver.db;
 
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.babynator.restserver.Baby;
 import com.babynator.restserver.Data;
 
 public class DataDAO {
@@ -36,7 +40,7 @@ public class DataDAO {
 	public static boolean addDataBirthday(Data data){
 		String requeteRegister = "INSERT INTO Data (weight,length,idbaby,currentdate) VALUES (?,?,?,(SELECT TO_DATE(?, 'DD-MM-YYYY') From DUAL))";
 		Date date = new Date();
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		DateFormat df = new SimpleDateFormat("dd/mm/yyyy");
 		System.out.println(df.format(date));
 		try {
 			PreparedStatement requeteSt = DAOOracle.getInstance().getConnection().prepareStatement(requeteRegister);
@@ -53,4 +57,24 @@ public class DataDAO {
 		}
 		return true;
 	}
+	
+	public static List<Data> getListData(int id_baby){
+		String requeteListData = "select idbaby, data_date, year, month, AVG(weight)as weight, AVG(length) as length from (select idbaby, to_char( currentdate , 'MM/YYYY' )as data_date, to_char(currentdate, 'yyyy') as year, to_char(currentdate, 'mm') as month,weight,length as length from data where idbaby = ?) group by idbaby, data_date,year, month order by year, month";
+		List<Data> datas = new ArrayList<Data>();
+		try {
+			PreparedStatement requeteSt = DAOOracle.getInstance().getConnection().prepareStatement(requeteListData);
+			requeteSt.setInt(1,id_baby);
+			ResultSet resultat = requeteSt.executeQuery();
+
+			while (resultat.next()) {
+				 datas.add(new Data(resultat.getInt("weight"), resultat.getInt("length") ,resultat.getInt("idbaby"),resultat.getString("data_date")));
+			}	
+		}
+		
+		catch (SQLException e) {
+
+		}
+		return datas;
+	}
+	
 }
--- RestServer/src/com/babynator/restserver/services/DataService.java
+++ RestServer/src/com/babynator/restserver/services/DataService.java
@@ -1,6 +1,7 @@
 package com.babynator.restserver.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
@@ -14,8 +15,10 @@ import javax.ws.rs.core.Response;
 import com.babynator.restserver.Baby;
 import com.babynator.restserver.BabyNatorUser;
 import com.babynator.restserver.Data;
+import com.babynator.restserver.Event;
 import com.babynator.restserver.db.BabyDAO;
 import com.babynator.restserver.db.DataDAO;
+import com.babynator.restserver.db.EventDAO;
 
 @Path("/datas")
 public class DataService {
@@ -37,10 +40,34 @@ public class DataService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response addData(Data data) {
     	System.out.println(data.toString());
+    	boolean testAddData = DataDAO.addData(data);
+    	if (!testAddData)
+    		return Response.status(Response.Status.CONFLICT).build();
+    	else 
+    		return Response.status(Response.Status.ACCEPTED).build();
+    }
+    
+    //liste des bébés
+    @POST
+    @Path("/addBirthday")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addDatabirthday(Data data) {
+    	System.out.println(data.toString());
     	boolean testAddData = DataDAO.addDataBirthday(data);
     	if (!testAddData)
     		return Response.status(Response.Status.CONFLICT).build();
     	else 
     		return Response.status(Response.Status.ACCEPTED).build();
+    }
+    
+    
+    @POST
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Data> getDatas(int id_baby) {
+    	System.out.println(id_baby);
+    	List<Data> list = DataDAO.getListData(id_baby);
+    	System.out.println(list);
+        return list; 
     }
 }
